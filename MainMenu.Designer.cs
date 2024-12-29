343447a1913c51ac9aaa5a42e14d8cb3f2194b5b
Artur Khankishyiev <arrivals240@gmail.com>
2024-12-29 15:31:27 +0100

    init

---
+++ MainMenu.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CheApp
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            calculateOxygenButton = new Button();
+            mmTitleTextBox = new TextBox();
+            SuspendLayout();
+            // 
+            // calculateOxygenButton
+            // 
+            calculateOxygenButton.Location = new Point(100, 87);
+            calculateOxygenButton.Name = "calculateOxygenButton";
+            calculateOxygenButton.Size = new Size(111, 23);
+            calculateOxygenButton.TabIndex = 0;
+            calculateOxygenButton.Text = "Calculate Oxygen";
+            calculateOxygenButton.UseVisualStyleBackColor = true;
+            calculateOxygenButton.Click += calculateOxygenButton_Click;
+            // 
+            // mmTitleTextBox
+            // 
+            mmTitleTextBox.BorderStyle = BorderStyle.None;
+            mmTitleTextBox.Cursor = Cursors.No;
+            mmTitleTextBox.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            mmTitleTextBox.Location = new Point(100, 40);
+            mmTitleTextBox.Name = "mmTitleTextBox";
+            mmTitleTextBox.ReadOnly = true;
+            mmTitleTextBox.Size = new Size(131, 32);
+            mmTitleTextBox.TabIndex = 1;
+            mmTitleTextBox.Text = "Main Menu";
+            mmTitleTextBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // MainMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(mmTitleTextBox);
+            Controls.Add(calculateOxygenButton);
+            Name = "MainMenu";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button calculateOxygenButton;
+        private TextBox mmTitleTextBox;
+    }
+}
---
+++ MainMenu.cs
@@ -0,0 +1,15 @@
+namespace CheApp
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateOxygenButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
